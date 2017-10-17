7ac7a29f876136c83e6a7efe76adbef453602b31
MarceloSSantos <marcelo.souza@dce.ufpb.br>
2017-10-17 13:42:06 -0300

    Uri atualizado dia 17/10/2017

--- GIT/src/URI1921.java
+++ GIT/src/URI1921.java
@@ -6,16 +6,16 @@ public class URI1921 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int resultado = (a - 3);
-		for (int i = (a - 3); i > 0; i--) {
-			resultado += i;
+		int n = scan.nextInt();
+		int x = (n - 3);
+		for (int i = (n - 3); i > 0; i--) {
+			x += i;
 		}
-		if (resultado < 0) {
-			resultado = 0;
+		if (x < 0) {
+			x = 0;
 		}
-
-		System.out.println(resultado);
+		System.out.println(x);
+		scan.close();
 
 	}
 }
